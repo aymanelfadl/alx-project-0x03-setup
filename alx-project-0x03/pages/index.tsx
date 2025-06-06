e258cd2ef9b046236facdd6dc0ac71991d154bd4
ayman elfadl <aymanelfadl16@gmail.com>
2025-06-06 19:31:04 +0100

    ok

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/layouts/Layout";
+
+export default function HomePage()
+{
+  return (
+    <Layout>
+      <div>
+        this home page
+      </div>
+    </Layout>
+  )
+}
